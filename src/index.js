f08a1a60e43f000c8f771eb7db20378cb412968b
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-04-09 12:34:50 -0400

    Buttons
    Changed button design, added a reset button

--- src/index.js
+++ src/index.js
@@ -16,18 +16,33 @@ class CountingParent extends React.Component {
     });
   };
 
+  resetAction = () => {
+    this.setState({
+      actionCount: 0
+    });
+  };
+
   render() {
     return (
       <div>
         <Child onAction={this.handleAction} />
         <p>Clicked {this.state.actionCount} times</p>
+        <button onClick={this.resetAction}>Reset Button</button>
       </div>
     );
   }
 }
 
 function Child({ onAction }) {
-  return <button onClick={onAction}>Click Me</button>;
+  return <button onClick={onAction}>Click me </button>;
 }
+
+var App = () => {
+  return (
+    <div className="App">
+      <CountingParent />
+    </div>
+  );
+};
 const rootElement = document.getElementById("root");
-ReactDOM.render(<CountingParent />, rootElement);
+ReactDOM.render(<App />, rootElement);
